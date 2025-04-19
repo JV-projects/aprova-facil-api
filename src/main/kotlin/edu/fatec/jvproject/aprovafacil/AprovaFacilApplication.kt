9bb42134d75bb14396a878ef118b15f417036f77
jackelinepaula <p.aulajackeline03@gmail.com>
2025-04-19 10:13:45 -0300

    feat: Projeto base

---
+++ src/main/kotlin/edu/fatec/jvproject/aprovafacil/AprovaFacilApplication.kt
@@ -0,0 +1,11 @@
+package edu.fatec.jvproject.aprovafacil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AprovaFacilApplication
+
+fun main(args: Array<String>) {
+	runApplication<AprovaFacilApplication>(*args)
+}
---
+++ src/test/kotlin/edu/fatec/jvproject/aprovafacil/AprovaFacilApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.fatec.jvproject.aprovafacil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AprovaFacilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
