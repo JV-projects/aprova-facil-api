4e5aba9f1b4b3eddb8874abc1a43250f430eb273
jackelinepaula <p.aulajackeline03@gmail.com>
2025-06-03 10:46:52 -0300

    feat: Implementando a devolutiva e controle do corretor

---
+++ src/main/kotlin/edu/fatec/jvproject/aprovafacil/controller/CorretorController.kt
@@ -0,0 +1,26 @@
+package edu.fatec.jvproject.aprovafacil.controller
+
+import edu.fatec.jvproject.aprovafacil.dto.DevolutivaRequest
+import edu.fatec.jvproject.aprovafacil.model.Atendimento
+import edu.fatec.jvproject.aprovafacil.service.AtendimentoService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/corretor")
+class CorretorController(
+    private val atendimentoService: AtendimentoService
+) {
+
+    @PostMapping("/devolutiva")
+    fun registrarDevolutiva(@RequestBody devolutivaRequest: DevolutivaRequest) : ResponseEntity<Atendimento>{
+        atendimentoService.registrarDevolutiva(
+            devolutivaRequest.codigo,
+            devolutivaRequest.devolutiva)
+
+        return ResponseEntity.ok().build()
+    }
+}
